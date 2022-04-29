b35d1145d568f68223f263ac15e0e0c118dc6397
Sofia <sofiiaglz@hotmail.com>
2022-04-29 16:06:24 -0500

    Cambios

--- app/Http/Controllers/HabitacionesController.php
+++ app/Http/Controllers/HabitacionesController.php
@@ -78,7 +78,7 @@ class HabitacionesController extends Controller
         $myVariable = $objeto->añadirHab($arr['nombre_habitacion']);
         }
         //Respuesta en caso de que todo vaya bien.
-        return response()->toArray([
+        return response()->json([
            /* 'message' => 'Habitaciones registradas',*/
          /**/   'datadetins' => $array,
            /* 'ada'=>$myVariable*/
